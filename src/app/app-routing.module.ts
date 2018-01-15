2082570be62f044d49a7882013721ef3b96055a7
Martin Atlantico Rodriguez <martinrodriguez493@hotmail.com>
2018-01-15 10:48:14 -0300

    creacion de primeras rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InicioComponent} from './components/inicio/inicio.component';
+import {ContactoComponent} from './components/contacto/contacto.component';
+import {PaginaDeErrorComponent} from './components/pagina-de-error/pagina-de-error.component';
+import {ImagenComponent} from './components/imagen/imagen.component';
+import {SeccionComponent} from './components/seccion/seccion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'imagen/:id', component: ImagenComponent},
+  {path: 'seccion/:id', component: SeccionComponent},
+  {path: 'error', component: PaginaDeErrorComponent},
+  {path: '**', redirectTo: 'error'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
